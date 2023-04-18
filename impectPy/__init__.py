eea7084a933cb78fc4f78332f9ce578e72988c5d
Florian Schmitt <florian.schmitt94@gmail.com>
2023-04-18 10:51:09 +0200

    add all modules as import

---
+++ impectPy/__init__.py
@@ -0,0 +1,6 @@
+# import modules
+from .access_token import getAccessToken
+from .competitions import getCompetitions
+from .match_plan import getMatchplan
+from .events import getEventData
+from .matchsums import getMatchsums
