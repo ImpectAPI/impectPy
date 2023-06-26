a83c558e6e7580634a992b373ad319a1c8abc806
Florian Schmitt <florian.schmitt94@gmail.com>
2023-06-26 15:38:26 +0200

    renamed competition.py to iterations.py, add helpers.py

--- impectPy/__init__.py
+++ impectPy/__init__.py
@@ -1,7 +1,8 @@
 # import modules
 from .access_token import getAccessToken
-from .competitions import getCompetitions
+from .iterations import getIterations
 from .match_plan import getMatchplan
 from .events import getEventData
 from .matchsums import getMatchsums
 from .sportscode_xml import generateSportsCodeXML
+from .helpers import make_api_request
