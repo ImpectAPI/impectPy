bc4f9288312871dbb0f5632b81eba997ed8c3a85
Florian Schmitt <florian.schmitt94@gmail.com>
2023-07-05 14:19:34 +0200

    add TokenBucket Class, RateLimitedAPI Class and process_response() function

--- impectPy/helpers.py
+++ impectPy/helpers.py
@@ -1,45 +1,188 @@
-######
-#
-# This function returns the response from an API call
-#
-######
-
 # load packages
 import requests
 import time
+import pandas as pd
+import re
+from typing import Optional, Dict, Any
+import math
 
 
-def make_api_request(url: str, method: str, headers: dict = None, data: dict = None,
-                     json: dict = None, session: requests.Session = None) -> requests.Response:
+######
+#
+# This class creates an object to handle rate-limited API requests
+#
+######
 
-    # create session object
-    with requests.Session() as session:
 
-        # define number of retries
-        max_retries = 3
+class RateLimitedAPI:
+    def __init__(self, session: Optional[requests.Session] = None):
+        """
+        Initializes a RateLimitedAPI object.
 
-        # define retry delay
-        retry_delay = 1
+        Args:
+            session (requests.Session): The session object to use for the API calls.
+        """
+        self.session = session or requests.Session()  # use the provided session or create a new session
+        self.bucket = None  # TokenBucket object to manage rate limit tokens
 
+    # make a rate-limited API request
+    def make_api_request_limited(self, url: str, method: str, headers: Optional[Dict[str, Any]] = None,
+                                 data: Optional[Dict[str, Any]] = None,
+                                 json: Optional[Dict[str, Any]] = None) -> requests.Response:
+        """
+        Executes an API call while applying the rate limit.
+
+        Returns:
+            requests.Response: The response returned by the API.
+        """
+
+        # check if bucket is not initialized
+        if not self.bucket:
+            # make an initial API call to get rate limit information
+            response = self.make_api_request(url=url, method=method, headers=headers, data=data, json=json)
+
+            # get rate limit policy
+            policy = response.headers["RateLimit-Policy"]
+
+            # extract maximum requests using regex
+            capacity = int(re.sub(";.*", "", policy))
+
+            # extract time window using regex
+            interval = int(re.sub(".*w=(\\d+).*", "\\1", policy))
+
+            # create TokenBucket
+            self.bucket = TokenBucket(
+                capacity=capacity,
+                refill_after=interval,
+                remaining=int(response.headers["RateLimit-Remaining"])
+            )
+
+            # return response
+            return response
+
+        # check if a token is available
+        if self.bucket.isTokenAvailable():
+            # get API response
+            response = self.make_api_request(url=url, method=method, headers=headers, data=data, json=json)
+
+            # consume a token
+            self.bucket.consumeToken()
+        else:
+            # wait for refill
+            time.sleep(
+                math.ceil(
+                    self.bucket.refill_after * 100 - (
+                            time.time() - self.bucket.last_refill_time
+                    ) *100
+                ) / 100
+            )
+
+            # call function again
+            response = self.make_api_request_limited(url=url, method=method, headers=headers, data=data, json=json)
+
+        # return response
+        return response
+
+    def make_api_request(self, url: str, method: str, headers: Optional[Dict[str, Any]] = None,
+                         data: Optional[Dict[str, Any]] = None,
+                         json: Optional[Dict[str, Any]] = None, max_retries: int = 3,
+                         retry_delay: int = 1) -> requests.Response:
+        """
+        Executes an API call.
+
+        Returns:
+            requests.Response: The response returned by the API.
+        """
         # try API call
         for i in range(max_retries):
-            # execute GET method
-            if method == 'GET':
-                response = session.get(url, headers=headers)
-            # execute POST method
-            elif method == 'POST':
-                response = session.post(url=url, headers=headers, data=data, json=json)
-            # raise exception
-            else:
-                raise ValueError(f"Unsupported HTTP method: {method}")
+            response = self.session.request(method=method, url=url, headers=headers, data=data, json=json)
 
             # check status code and return if 200
             if response.status_code == 200:
+                # return response
                 return response
+
             # raise exception
             else:
                 print(f"Received status code {response.status_code}, retrying in {retry_delay} seconds...")
                 time.sleep(retry_delay)
 
-        # return response
-        return response
+
+######
+#
+# This class creates a token bucket that handles the rate limit returned by the API accordingly
+#
+######
+
+
+class TokenBucket:
+    def __init__(self, capacity: int, refill_after: int = 1, remaining: int = 0):
+        """
+        Initializes a TokenBucket object.
+
+        Args:
+            capacity (int): The maximum number of tokens the bucket can hold.
+            refill_after (int): The time period (in seconds) after which the bucket is refilled.
+            remaining (int): The amount of tokens remaining at the moment of initialization.
+        """
+        self.capacity = capacity  # maximum number of tokens the bucket can hold
+        self.refill_after = refill_after  # time period (in seconds) after which the bucket is refilled
+        self.tokens = remaining  # number of tokens remaining at time of bucket creation
+        self.last_refill_time = time.time()  # time of the last token refill
+
+    def addTokens(self):
+        """
+        Refills the token bucket if the refill time has elapsed.
+        """
+        now = time.time()  # current time
+        elapsed_time = now - self.last_refill_time  # time elapsed since the last token refill
+        if elapsed_time > self.refill_after:
+            self.tokens = self.capacity  # refill the bucket to its maximum capacity
+            self.last_refill_time = now  # update the last refill time to the current time
+
+    def isTokenAvailable(self):
+        """
+        Checks if at least one token is available in the bucket.
+
+        Returns:
+            bool: True if a token is available, False otherwise.
+        """
+        self.addTokens()  # ensure the token bucket is up-to-date
+        return self.tokens >= 1  # return True if there is at least one token, False otherwise
+
+    def consumeToken(self):
+        """
+        Consumes a token from the bucket if available.
+
+        Returns:
+            bool: True if a token was consumed successfully, False otherwise.
+        """
+        if not self.isTokenAvailable():  # if no token is available, return False
+            return False
+        self.tokens -= 1  # decrement the token count by 1
+        return True  # return True to indicate successful token consumption
+
+
+######
+#
+# This function converts the response from an API call to a pandas dataframe, flattens it and fixes the column names
+#
+######
+
+
+def process_response(self: requests.Response) -> pd.DataFrame:
+    # get data from response
+    result = self.json()["data"]
+
+    # convert to df
+    result = pd.json_normalize(result)
+
+    # fix column names using regex
+    result = result.rename(columns=lambda x: re.sub("\.(.)", lambda y: y.group(1).upper(), x))
+
+    # return result
+    return result
+
+
+# attach method to requests module
+requests.Response.process_response = process_response
