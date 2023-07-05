3911bfe3f3235aae1b8f15197894858b5fbe0eae
Florian Schmitt <florian.schmitt94@gmail.com>
2023-07-05 14:16:46 +0200

    add TokenBucket logic

--- impectPy/access_token.py
+++ impectPy/access_token.py
@@ -1,18 +1,19 @@
+# load packages
+import urllib
+from impectPy.helpers import RateLimitedAPI
+
 ######
 #
 # This function returns an access token for the external API
 #
 ######
 
-# load packages
-import requests
-import time
-import urllib
-from impectPy.helpers import make_api_request
-
 
 # define function
 def getAccessToken(username: str, password: str) -> str:
+    # create an instance of RateLimitedAPI
+    rate_limited_api = RateLimitedAPI()
+
     # create tokenURL
     token_url = "https://login.impect.com/auth/realms/release/protocol/openid-connect/token"
 
@@ -25,11 +26,8 @@ def getAccessToken(username: str, password: str) -> str:
                "Content-Type": "application/x-www-form-urlencoded"}
 
     # request access token
-    response = make_api_request(url=token_url, method="POST", headers=headers, data=login, json=None)
+    response = rate_limited_api.make_api_request(url=token_url, method="POST", headers=headers, data=login, json=None)
 
-    # raise an HTTPError for a non-200 status code
-    response.raise_for_status()
-
-    # get access token from response
+    # get access token from response and return it
     token = response.json()["access_token"]
     return token
