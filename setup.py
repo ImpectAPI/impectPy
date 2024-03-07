f3032712ad43e75b137e4e65db0e0b95e8ddf859
Florian Schmitt <50988253+Flosch1006@users.noreply.github.com>
2024-03-07 14:34:50 +0100

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
                       "pandas>=2.0.0",
                       "numpy>=1.24.2"],
     # *strongly* suggested for sharing
-    version="2.0.4",
+    version="2.0.5",
     # The license can be anything you like
     license="MIT",
     description="A Python package to facilitate interaction with the Impect customer API",
