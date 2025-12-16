bab31b8fc5a084f038241603f4c133229f866496
Florian Schmitt <florian.schmitt94@gmail.com>
2025-12-16 15:42:24 +0100

    split functions into one function per file

--- impectPy/__init__.py
+++ impectPy/__init__.py
@@ -6,15 +6,21 @@ from .access_token import getAccessToken
 from .iterations import getIterations
 from .matches import getMatches
 from .events import getEvents
-from .matchsums import getPlayerMatchsums, getSquadMatchsums
-from .iteration_averages import getPlayerIterationAverages, getSquadIterationAverages
-from .player_scores import getPlayerMatchScores, getPlayerIterationScores
-from .squad_scores import getSquadMatchScores, getSquadIterationScores
+from .player_matchsums import getPlayerMatchsums
+from .squad_matchsums import getSquadMatchsums
+from .player_iteration_averages import getPlayerIterationAverages
+from .squad_iteration_averages import getSquadIterationAverages
+from .player_match_scores import getPlayerMatchScores
+from .player_iteration_scores import getPlayerIterationScores
+from .squad_match_scores import getSquadMatchScores
+from .squad_iteration_scores import getSquadIterationScores
 from .player_profile_scores import getPlayerProfileScores
 from .generate_xml import generateXML
 from .set_pieces import getSetPieces
 from .squad_ratings import getSquadRatings
 from .squad_coefficients import getSquadCoefficients
-from .match_info import getFormations, getSubstitutions, getStartingPositions
+from .formations import getFormations
+from .substitutions import getSubstitutions
+from .starting_positions import getStartingPositions
 from .config import Config as Config
 from .impect import Impect as Impect
