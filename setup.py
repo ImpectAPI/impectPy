3cb75fe4e10cfc94be6180e9fa365ee32b0ce21b
hugoriosneto <40169944+hugoriosneto@users.noreply.github.com>
2026-03-02 11:21:49 +0100

    update setup.py metadata for impectPyRSCA

--- setup.py
+++ setup.py
@@ -5,22 +5,16 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"))
     README = f.read()
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
-    name="impectPy",
-    url="https://github.com/ImpectAPI/impectPy",
-    author="Impect",
-    author_email="info@impect.com",
-    # Needed to actually package something
-    packages=["impectPy"],
-    # Needed for dependencies
+    name="impectPyRSCA",
+    url="https://github.com/rsca-intelligence/impectPyRSCA",
+    author="RSCA Intelligence",
+    packages=["impectPyRSCA"],
     install_requires=["requests>=2.24.0",
                       "pandas>=2.2.0",
                       "numpy>=1.24.2"],
-    # *strongly* suggested for sharing
     version="2.5.7",
-    # The license can be anything you like
     license="MIT",
-    description="A Python package to facilitate interaction with the Impect customer API",
+    description="RSCA fork of impectPy — a Python package to facilitate interaction with the Impect customer API",
     long_description=README,
     long_description_content_type="text/markdown",
 )
